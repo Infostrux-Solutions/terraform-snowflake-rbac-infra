43848eb20bd2c80966a1facc235d9a2ff8dd40b7
mvanzanten <matt@infostrux.com>
2023-02-28 15:48:21 -0800

    Update warehouse.tf

--- terraform/02-snowflake-infrastructure/warehouse.tf
+++ terraform/02-snowflake-infrastructure/warehouse.tf
@@ -2,8 +2,10 @@ module "sf_warehouse" {
   source   = "../../modules/sf_warehouse"
   for_each = var.warehouses_and_roles
 
-  name           = upper(join("_", [var.customer, var.environment, each.key]))
-  warehouse_size = var.snowflake_warehouse_size
+  name = upper(join("_", [var.customer, var.environment, each.key]))
+
+  warehouse_size         = var.snowflake_warehouse_size
+  warehouse_auto_suspend = try(var.warehouse_auto_suspend[each.key], 600)
 
   tags        = merge(var.default_tags, { "WAREHOUSE_PURPOSE" = var.warehouse_tags[each.key] })
   tags_db     = "DB_TAGS"
