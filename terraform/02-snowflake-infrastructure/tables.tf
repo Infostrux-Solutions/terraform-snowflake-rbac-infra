0a771a7d8105962cb2a593d5e691cebe05bdf546
Matt Van Zanten <matt@archer-2.local>
2024-04-12 16:56:45 -0700

    simplify ownership

--- terraform/02-snowflake-infrastructure/tables.tf
+++ terraform/02-snowflake-infrastructure/tables.tf
@@ -22,11 +22,30 @@ resource "snowflake_grant_privileges_to_account_role" "tables" {
     for uni in local.table_grants_wo_ownership : uni.unique => uni
   }
 
-  provider = snowflake.securityadmin
+  provider   = snowflake.securityadmin
+  depends_on = [snowflake_grant_ownership.tables]
 
   account_role_name = each.value.role
   privileges        = each.value.privilege
   on_schema_object {
+    future {
+      object_type_plural = "TABLES"
+      in_database        = snowflake_database.database[each.value.database].id
+    }
+  }
+}
+
+resource "snowflake_grant_ownership" "tables" {
+  for_each = {
+    for uni in local.database_grants : uni.unique => uni if contains(uni.privilege, "ownership")
+  }
+
+  provider   = snowflake.securityadmin
+  depends_on = [snowflake_grant_account_role.role]
+
+  account_role_name   = each.value.role
+  outbound_privileges = "REVOKE"
+  on {
     future {
       object_type_plural = "TABLES"
       in_database        = snowflake_database.database[each.value.database].id
