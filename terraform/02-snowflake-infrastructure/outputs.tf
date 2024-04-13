04557da5f8722e64a93059226b425b3911074fa0
Matt Van Zanten <matt@archer-2.local>
2024-04-13 11:19:36 -0700

    add output

--- terraform/02-snowflake-infrastructure/outputs.tf
+++ terraform/02-snowflake-infrastructure/outputs.tf
@@ -1,11 +1,17 @@
 output "database_names" {
   value = [
-    for db in snowflake_database.database : db.name
+    for database in snowflake_database.database : database.name
   ]
 }
 
 output "warehouse_names" {
   value = [
-    for wh in snowflake_warehouse.warehouse : wh.name
+    for warehouse in snowflake_warehouse.warehouse : warehouse.name
+  ]
+}
+
+output "role_grants" {
+  value = [
+    for role in snowflake_grant_account_role.role : join("", [role.role_name, " ▶ ", role.parent_role_name])
   ]
 }
