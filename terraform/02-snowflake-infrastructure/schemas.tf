ee21f318cc2b71bf51ccc47b981b7579f749e4c3
Matt Van Zanten <matt@archer-2.local>
2024-04-12 17:07:03 -0700

    simplify ownership

--- terraform/02-snowflake-infrastructure/schemas.tf
+++ terraform/02-snowflake-infrastructure/schemas.tf
@@ -5,6 +5,17 @@ locals {
     for database, grants in local.schema_yml.schemas : database => grants
   }
 
+  schema_grants = flatten([
+    for database, grants in local.schemas : [
+      for role, privilege in grants : {
+        unique    = join("_", [database, trimspace(role)])
+        database  = database
+        privilege = privilege
+        role      = upper(join("_", [var.customer, var.environment, role]))
+      }
+    ]
+  ])
+
   schema_grants_wo_ownership = flatten([
     for database, grants in local.schemas : [
       for role, privilege in grants : {
@@ -22,11 +33,30 @@ resource "snowflake_grant_privileges_to_account_role" "schema" {
     for uni in local.schema_grants_wo_ownership : uni.unique => uni
   }
 
-  provider = snowflake.securityadmin
+  provider   = snowflake.securityadmin
+  depends_on = [snowflake_grant_ownership.views]
 
   account_role_name = each.value.role
   privileges        = each.value.privilege
   on_schema {
     future_schemas_in_database = snowflake_database.database[each.value.database].id
+  }
+}
+
+resource "snowflake_grant_ownership" "schemas" {
+  for_each = {
+    for uni in local.schema_grants : uni.unique => uni if contains(uni.privilege, "ownership")
+  }
+
+  provider   = snowflake.securityadmin
+  depends_on = [snowflake_grant_account_role.role]
+
+  account_role_name   = each.value.role
+  outbound_privileges = "REVOKE"
+  on {
+    future {
+      object_type_plural = "SCHEMAS"
+      in_database        = snowflake_database.database[each.value.database].id
+    }
   }
 }
--- terraform/02-snowflake-infrastructure/views.tf
+++ terraform/02-snowflake-infrastructure/views.tf
@@ -5,6 +5,17 @@ locals {
     for database, grants in local.view_yml.views : database => grants
   }
 
+  view_grants = flatten([
+    for database, grants in local.views : [
+      for role, privilege in grants : {
+        unique    = join("_", [database, trimspace(role)])
+        database  = database
+        privilege = privilege
+        role      = upper(join("_", [var.customer, var.environment, role]))
+      }
+    ]
+  ])
+
   view_grants_wo_ownership = flatten([
     for database, grants in local.views : [
       for role, privilege in grants : {
@@ -22,11 +33,30 @@ resource "snowflake_grant_privileges_to_account_role" "views" {
     for uni in local.view_grants_wo_ownership : uni.unique => uni
   }
 
-  provider = snowflake.securityadmin
+  provider   = snowflake.securityadmin
+  depends_on = [snowflake_grant_ownership.views]
 
   account_role_name = each.value.role
   privileges        = each.value.privilege
   on_schema_object {
+    future {
+      object_type_plural = "VIEWS"
+      in_database        = snowflake_database.database[each.value.database].id
+    }
+  }
+}
+
+resource "snowflake_grant_ownership" "views" {
+  for_each = {
+    for uni in local.view_grants : uni.unique => uni if contains(uni.privilege, "ownership")
+  }
+
+  provider   = snowflake.securityadmin
+  depends_on = [snowflake_grant_account_role.role]
+
+  account_role_name   = each.value.role
+  outbound_privileges = "REVOKE"
+  on {
     future {
       object_type_plural = "VIEWS"
       in_database        = snowflake_database.database[each.value.database].id
