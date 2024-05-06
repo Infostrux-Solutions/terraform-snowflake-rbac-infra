5aa44b781b89d1a2f47584aa94860a77f4b59896
Matt Van Zanten <matt@archer.local>
2024-05-06 12:06:12 -0700

    adding users by config

--- grants.tf
+++ grants.tf
@@ -44,23 +44,14 @@ resource "snowflake_grant_account_role" "account_role" {
   parent_role_name = each.value.parent
 }
 
-resource "snowflake_grant_account_role" "fivetran" {
-  count = var.create_fivetran_user ? 1 : 0
-
-  provider = snowflake.securityadmin
-
-  role_name = snowflake_role.environment_role["ingestion"].name
-  user_name = snowflake_user.fivetran[0].name
-}
-
-resource "snowflake_grant_account_role" "datadog" {
-  count = var.create_datadog_user ? 1 : 0
+resource "snowflake_grant_account_role" "user" {
+  for_each = local.users
 
   provider   = snowflake.securityadmin
   depends_on = [snowflake_role.environment_role]
 
-  role_name = snowflake_role.environment_role["monitoring"].name
-  user_name = snowflake_user.datadog[0].name
+  role_name = snowflake_role.environment_role[each.value.role].name
+  user_name = snowflake_user.user[each.key].name
 }
 
 resource "snowflake_grant_privileges_to_account_role" "tag_database" {
--- users.tf
+++ users.tf
@@ -1,78 +1,29 @@
 locals {
-  dbt_username       = upper(join("_", [local.object_prefix, "dbt"]))
-  fivetran_username  = upper(join("_", [local.object_prefix, "fivetran"]))
-  datadog_username   = upper(join("_", [local.object_prefix, "datadog"]))
-  reporting_username = upper(join("_", [local.object_prefix, "reporting"]))
+  users = {
+    for user, specs in local.users_yml.users : join("_", [local.object_prefix, user]) => specs
+  }
 }
 
-resource "snowflake_user" "dbt" {
-  count = var.create_dbt_user ? 1 : 0
+resource "snowflake_user" "user" {
+  for_each = local.users
 
   provider = snowflake.securityadmin
 
-  name           = local.dbt_username
-  login_name     = local.dbt_username
-  rsa_public_key = var.snowflake_dbt_private_key
-  display_name   = local.dbt_username
-  comment        = var.comment
-  disabled       = false
+  name         = each.key
+  login_name   = each.key
+  display_name = each.key
+  comment      = var.comment
+  disabled     = false
 
-  default_warehouse = snowflake_warehouse.warehouse["transform"].name
-  default_role      = snowflake_role.environment_role["transform"].name
-
+  default_warehouse    = snowflake_warehouse.warehouse[each.value.warehouse].name
+  default_role         = snowflake_role.environment_role[each.value.role].name
   must_change_password = false
-}
 
-resource "snowflake_user" "fivetran" {
-  count = var.create_fivetran_user ? 1 : 0
-
-  provider = snowflake.securityadmin
-
-  name           = local.fivetran_username
-  login_name     = local.fivetran_username
-  rsa_public_key = var.snowflake_fivetran_private_key
-  display_name   = local.fivetran_username
-  comment        = var.comment
-  disabled       = false
-
-  default_warehouse = snowflake_warehouse.warehouse["ingestion"].name
-  default_role      = snowflake_role.environment_role["ingestion"].name
-
-  must_change_password = false
-}
-
-resource "snowflake_user" "datadog" {
-  count = var.create_datadog_user ? 1 : 0
-
-  provider = snowflake.securityadmin
-
-  name           = local.datadog_username
-  login_name     = local.datadog_username
-  rsa_public_key = var.snowflake_datadog_private_key
-  display_name   = local.datadog_username
-  comment        = var.comment
-  disabled       = false
-
-  default_warehouse = snowflake_warehouse.warehouse["monitoring"].name
-  default_role      = snowflake_role.environment_role["monitoring"].name
-
-  must_change_password = false
-}
-
-resource "snowflake_user" "reporting" {
-  count = var.create_reporting_user ? 1 : 0
-
-  provider = snowflake.securityadmin
-
-  name           = local.dbt_username
-  login_name     = local.dbt_username
-  rsa_public_key = var.snowflake_reporting_private_key
-  display_name   = local.dbt_username
-  comment        = var.comment
-  disabled       = false
-
-  default_warehouse = snowflake_warehouse.warehouse["reporting"].name
-  default_role      = snowflake_role.environment_role["analyst"].name
-
-  must_change_password = false
+  lifecycle {
+    ignore_changes = [
+      password,
+      rsa_public_key,
+      rsa_public_key_2
+    ]
+  }
 }
